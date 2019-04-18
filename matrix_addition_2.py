bfc09285ae9f01ecd7eab2088214162a1dab1a2c
RJ Salamanca <salamancaronito@gmail.com>
2019-04-18 09:40:59 -0400

    added matrix addition 2

---
+++ matrix_addition_2.py
@@ -0,0 +1,11 @@
+list_2d_1 = [ [1,3,1,3], [2,4] ]
+list_2d_2 = [ [5,2,5,2], [1,0] ]
+list_answer = []
+
+for i in range(len(list_2d_1)):
+    temp_added_list = []
+    for j in range(len(list_2d_1[i])):
+        temp_added_list.append( list_2d_1[i][j] + list_2d_2[i][j])
+    list_answer.append(temp_added_list)
+
+print(list_answer)
