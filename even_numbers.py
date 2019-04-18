da5bae110a55e7d86a6c8006020fef8300ec5df9
RJ Salamanca <salamancaronito@gmail.com>
2019-04-18 09:21:12 -0400

    removed append

--- even_numbers.py
+++ even_numbers.py
@@ -1,8 +1,5 @@
 list_numbers = [1,2,3,4,5,6,7,8,9,10]
-evens = []
 
 for i in range(len(list_numbers)):
     if list_numbers[i] % 2 == 0:
-        evens.append(list_numbers[i])
-
-print(evens)
+        print(list_numbers[i])
