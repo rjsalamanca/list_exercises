0c00422dfeb420af079ce61fb7ad6bc108a2f44f
RJ Salamanca <salamancaronito@gmail.com>
2019-04-18 09:13:41 -0400

    added smallest number

---
+++ smallest_number.py
@@ -0,0 +1,15 @@
+list_numbers = [1,2,40,3,4,5,6]
+
+min_number = 0
+
+for i in range(len(list_numbers)):
+    if i == 0:
+        min_number = list_numbers[i]
+    elif list_numbers[i] < min_number:
+        min_number = list_numbers[i]
+
+print(min_number)
+
+# The function below min() will search through the list and return the smallest number
+
+#print(min(list_numbers))
